97e64d7dfab312f1634eb5a05763e8a25ad8c00e
eloco <exia@isimarkets.com>
2021-09-01 15:58:12 +0800

    init action

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.9-alpine
-COPY LICENSE README.md /
-COPY entrypoint.sh /entrypoint.sh
-COPY requirements.txt /requirements.txt
-COPY main_skype.py /main_skype.py
+COPY LICENSE README.md  /
+COPY main_skype.py      /
+COPY requirements.txt   /
+COPY entrypoint.sh      /
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -l
 #if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
 pip install -r requirements.txt
-python  main_skype.py   --login_name=           "$INPUT_SKYPE_USERNAME" \
-                        --login_password=       "$INPUT_SKYPE_PASSWORD"\
-                        --skype_ids=            "$INPUT_SKYPE_IDS"\
-                        --send_msg_path=        "$INPUT_SEND_MSG_PATH"\
-                        --send_file_path=       "$INPUT_SEND_FILE_PATH"
+ls
+python main_skype.py 

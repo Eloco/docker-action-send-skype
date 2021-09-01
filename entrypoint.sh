3fac3f2078b41479fb8c4533dcd497cbc46076cd
eloco <exia@isimarkets.com>
2021-09-01 16:23:36 +0800

    init action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 wget -O action_main.py      https://raw.githubusercontent.com/Eloco/docker-action-send-skype/main/action_main.py
-wget -O requirements.txt    https://raw.githubusercontent.com/Eloco/docker-action-send-skype/main/requirements.py
+wget -O requirements.txt    https://raw.githubusercontent.com/Eloco/docker-action-send-skype/main/requirements.txt
 pip install -r requirements.txt
 python action_main.py 

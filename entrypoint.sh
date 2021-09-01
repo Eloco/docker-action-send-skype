4f3d95411e6a9381baa07b0a3f0db068b8ecc233
eloco <exia@isimarkets.com>
2021-09-01 09:06:59 +0800

    init action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -l
 
 echo "hello $1 $GITHUB_WORKSPACE"
+ls $GITHUB_WORKSPACE
 pip -V
 python -V
 
-if true ; then
-  echo "Game over!"
-  exit 1
-fi
+#if true ; then
+#  echo "Game over!"
+#  exit 1
+#fi

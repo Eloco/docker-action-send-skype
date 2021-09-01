28d39f15feff529686932d3d4ae994a60f878d62
eloco <exia@isimarkets.com>
2021-09-01 13:09:43 +0800

    init action

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM python:3.9-alpine
 
 COPY LICENSE README.md /
 COPY entrypoint.sh /entrypoint.sh
-
+COPY requirements.txt /requirements.txt
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh -l
-
-echo "hello $1 $GITHUB_WORKSPACE"
-ls $GITHUB_WORKSPACE
-pip -V
-python -V
-
+if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
 #if true ; then
 #  echo "Game over!"
 #  exit 1
